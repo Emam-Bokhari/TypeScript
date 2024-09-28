c939c4a1bf9d00d09d17dd6c8625b1b0b63c8875
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-28 10:05:19 +0600

    feat(types): add Typescript variables and function with type annotations

---
+++ firstProgram/index.js
@@ -0,0 +1,10 @@
+var user = "Emam Bokhari";
+console.log(user);
+var age = 25;
+console.log(age);
+var email = "moshfiqurrahman37@gmail.com";
+console.log(email);
+function sum(value1, value2) {
+    console.log(value1 + value2);
+}
+sum(5, 5);
---
+++ firstProgram/index.ts
@@ -0,0 +1,14 @@
+let user: string = "Emam Bokhari";
+console.log(user);
+
+const age: number = 25;
+console.log(age);
+
+const email: string = "moshfiqurrahman37@gmail.com"
+console.log(email);
+
+function sum(value1:number, value2:number) {
+    console.log(value1 + value2);
+}
+
+sum(5,5)
