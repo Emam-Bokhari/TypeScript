5359f2c3ab51b79e54bfb1a998c6c7619e9897bf
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-28 11:07:52 +0600

    feat(types): use union types with variables and functions in Typescript

---
+++ unionDataType/index.ts
@@ -0,0 +1,14 @@
+// aker odhik data type niye kaj korte chaile amra union use korte pari, amra function a o union use korte pari
+// amra chaile union a parenthesis use korte pari abar chaile parenthesis chara o use korte pari
+
+const userId: (number | string) = 211;
+console.log(userId);
+
+const activeStatus: string | boolean = true;
+console.log(activeStatus)
+
+function displayUserInfo(userId: number | string) {
+    console.log(userId)
+}
+
+displayUserInfo(20)
