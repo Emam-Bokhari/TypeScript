7d447624b05e93ad7e9e428b75f6b2c9167aa02b
Moshfiqur Rahman <studentemam@gmail.com>
2024-09-28 10:24:13 +0600

    feat(types): show Typescript types and catch errors easyly

---
+++ builtInType/index.js
@@ -0,0 +1,16 @@
+//  build in types: string, number, boolean, void, undefined, null
+// any data type
+var id;
+// number data type
+var userId = 211;
+console.log(userId);
+// string data type
+var userName = "Emam Bokhari";
+console.log(userName);
+// boolean data type
+var userStatus = true;
+console.log(userStatus);
+// aikhne uderId hoche number, r userStatus hoche boolean ja kina toLowerCase kora jai na, jar jonno age e she error throw kortese, amra arokom aro onk subidha peye thaki typescript use korar fole
+console.log(userId.toLowerCase());
+console.log(userName.toLowerCase());
+console.log(userStatus.toLowerCase());
