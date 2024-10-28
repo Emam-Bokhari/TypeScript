038985eb9e9c6164a3b41837ddde182bcd66da26
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-28 11:45:46 +0600

    feat: add examples of type assertions

--- src/learn-types.ts
+++ src/learn-types.ts
@@ -13,3 +13,11 @@ let multipleTypesEx2: string | number = "20";
 multipleTypesEx2 = 30;
 
 
+const str:string="Emam Bokhari is my good name.";
+
+const index:number=(str as string).indexOf("B");
+
+const indexEx2:number=(<string>str).indexOf("E");
+
+console.log(index,indexEx2)
+
