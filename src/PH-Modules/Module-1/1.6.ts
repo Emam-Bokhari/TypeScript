012aff67798c12af45f6a3294bcac2a7c2dbf48d
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-30 21:58:53 +0600

    feat: add example of function, arrow function, methods, and array map

---
+++ src/PH-Modules/Module-1/1.6.ts
@@ -0,0 +1,37 @@
+// function, arrow function, map, method
+
+// normal function 
+function addition(x: number, y: number = 0): number {
+    return x + y;
+};
+
+addition(2, 5);
+
+// arrow function
+const substraction = (x: number, y: number): number => {
+    return x - y;
+};
+
+substraction(33, 21);
+
+// method
+const poorUser: {
+    name: string,
+    age: number,
+    balance: number,
+    addBalance: (newBalance: number) => number,
+} = {
+    name: "Emam Bokhari",
+    age: 24,
+    balance: 0,
+    addBalance(newBalance: number): number {
+        return this.balance + newBalance;
+    },
+};
+
+// map
+const itemsOfFlower:number[]=[2,4,9];
+
+const newItemsOfFlower:number[]=itemsOfFlower.map((item:number):number=>item*2);
+
+console.log(newItemsOfFlower)
