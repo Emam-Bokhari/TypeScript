2756087b8301e851ec08ed184475bea138f2248e
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-30 20:27:51 +0600

    feat: add examples of tuple,arays, and andy data types

--- src/PH-Modules/Module-1/module-1.ts
+++ src/PH-Modules/Module-1/module-1.ts
@@ -14,3 +14,25 @@ const profileStatus: null = null;
 
 // undefined
 const x: undefined = undefined;
+
+// any data type , any type ar data type a js ar moto dynamic types ar data rakha jai
+let y: any = "20";
+y = 30;
+y = true;
+y = null;
+y = undefined;
+
+
+// # Non primitive datat type
+
+// array
+const friendsList: string[] = ["Abdul Jalil", "Abdul Malik", "Abdur Rahman"];
+
+const numberOfProducts: number[] = [2, 4, 5, 6, 11, 21];
+
+// tuple
+const coordinate: [number, number] = [1, 5];
+
+const info: [string, number, string] = ["Abdul Mannan", 24, "Bangladesh"];
+
+
