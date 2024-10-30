7efcf550ae3ee1b7598f1624a2029f3664051327
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-30 20:52:36 +0600

    add object types with literel,readonly and optional properties

---
+++ src/PH-Modules/Module-1/module-1.5.ts
@@ -0,0 +1,25 @@
+// # object, optional, literal types, readonly
+
+const patientsInfo: {
+    id: number;
+    name: string;
+    age: number;
+    weight: number;
+    medicalReport?: string; // optional types
+    address: string;
+    doctorName: string;
+    hospitalName:"Dreams Care Health Clinic Ltd", // literel types
+   readonly diagnonisticCenter:"Dreams Care Diagonistic Center" // readonly
+    status: boolean;
+} = {
+    id: 1001,
+    name: "Moshfiqur Rahman",
+    age: 24,
+    weight: 50,
+    address: "Brahmanbaria,Bangladesh",
+    doctorName: "Abdur Rahman",
+    hospitalName:"Dreams Care Health Clinic Ltd",
+    diagnonisticCenter:"Dreams Care Diagonistic Center",
+    status: true,
+}
+
