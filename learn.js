512b9f569e30260dea6fa7b287775427e1de483b
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-30 19:44:03 +0600

    docs: add fnm installation process and typescript file config command

---
+++ learn.js
@@ -0,0 +1,21 @@
+/* 
+fnm ar full meaning first node manager
+# Install fnm node version manager
+1. open powershell and command
+winget install Schniz.fnm
+fnm env --use-on-cd | Out-String | Invoke-Expression
+fnm use --install-if-missing 22
+2. check node version
+node -v
+3. install kora node ar somosto version dekhte 
+fnm list
+4. noton kono version install korte chaile 
+fnm install 20.18.0
+5. abar ager install kora version use korte chaile
+fnm use 22.11.0
+
+# typescript ar configuration file ar jonno
+tsc --init
+
+
+*/
