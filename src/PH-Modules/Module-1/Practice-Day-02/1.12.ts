eac55f1abfb94be242e96022409163f412d29fd6
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-04 21:03:33 +0600

    implement unknown, nullable, and never types with practical examples

--- src/PH-Modules/Module-1/Practice-Day-02/1.12.ts
+++ src/PH-Modules/Module-1/Practice-Day-02/1.12.ts
@@ -1,3 +1,33 @@
 {
-    // 
+    // nullable, unknown ang never types
+
+    const search = (value: string | null) => {
+        if (value) {
+            console.log("Search")
+        } else {
+            console.log("Nothing...")
+        }
+    }
+
+    search(null)
+
+    const KgToGm = (value: unknown) => {
+        if (typeof value === "number") {
+            return value / 1000;
+        } else if (typeof value === "string") {
+            const parts = value.split(" ")
+            const converted = parseFloat(parts[0]);
+            return `${converted / 1000} KG`;
+        }
+    }
+    const result = KgToGm("1200 Gm");
+    console.log(result);
+
+    const ErrorMessage = (message: string): never => {
+        throw new Error(message)
+    }
+
+    ErrorMessage("Error Occured!")
+
+
 }
