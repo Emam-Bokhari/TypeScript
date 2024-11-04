588abc5165fb7b9cbf44518827baf34dee20ad1e
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-04 18:54:03 +0600

    feat: add laptopInfo object with optional,literal, and readonly properties

---
+++ src/PH-Modules/Module-1/Practice-Day-02/1.5.ts
@@ -0,0 +1,18 @@
+{
+    // object, optional, literal , and readonly
+
+    const laptopInfo: {
+        readonly id: number;
+        brand: "Asus";
+        modelNumber: string;
+        weight: string;
+        color: string;
+        manufectureDate?: string;
+    } = {
+        id: 1001,
+        brand: "Asus",
+        modelNumber: "As-9925L",
+        weight: "2.7 KG",
+        color: "Silver"
+    }
+}
