04dbb0e0d1b176db9114babf750139de14958e4f
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-05 23:05:07 +0600

    feat: implemnts spread operator examples of dataOfStudents, dataOfFullDetails, animalList, and fullListOfAnimal

--- src/PH-Modules/Conceptual-Seccion/Session-1/Part-2/index.ts
+++ src/PH-Modules/Conceptual-Seccion/Session-1/Part-2/index.ts
@@ -17,9 +17,35 @@
     }
 
     const { id, ...othersData } = users;
-    // console.log(id, othersData);
 
-    
+    // spread operator
+    type DataOfStudents = {
+        id: number;
+        name: string;
+        age: number;
+        email?: string;
+    }
+    const dataOfStudents: DataOfStudents = {
+        id: 2050,
+        name: "Moshfiqur Rahman",
+        age: 24,
+    };
+
+    const dataOfFullDetails: DataOfStudents = {
+        ...dataOfStudents,
+        email: "studentemam@gmail.com",
+    };
+
+    console.log(dataOfFullDetails);
+
+    const animalList: string[] = ["Cat", "Horse", "Cow"];
+
+    const fullListOfAnimal: string[] = [...animalList, "Monkey"];
+
+
+
+
+
 
 
 
