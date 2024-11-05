3b072139b7f81ffefa7617a5e15dd32f8c02838c
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-05 20:18:37 +0600

    feat: add examples of nonprimitive data type, such as object, array, and tuples

--- src/PH-Modules/Conceptual-Seccion/Session-1/Part-1/index.ts
+++ src/PH-Modules/Conceptual-Seccion/Session-1/Part-1/index.ts
@@ -1,5 +1,5 @@
 {
-    // primitive, nonprimitive, object, array, tuple, function, arrow function, type alias
+    // primitive, nonprimitive (object, array, tuple, function, arrow function), type alias
 
     // primitive data types
 
@@ -12,4 +12,27 @@
     const userData: null = null;
 
     const userBalance: undefined = undefined;
+
+    // non primitive data type
+
+    const userInfo: {
+        id: number;
+        name: string;
+        age: number;
+        isActive: boolean;
+    } = {
+        id: 1580,
+        name: "Moshfiqur Rahman",
+        age: 24,
+        isActive: true,
+    }
+
+    const fruits: string[] = ["Mango", "Banana", "Apple"];
+
+    const fruitsPrice: number[] = [320, 120, 140];
+
+    const userNameAndAge: [string, number, string, number] = ["Moshfiqur Rahman", 24, "Imtiaz Hossain Akib", 21];
+
+    
+
 }
