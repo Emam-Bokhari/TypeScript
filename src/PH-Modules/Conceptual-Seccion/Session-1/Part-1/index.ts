ad2a022242c17bf1bc27d7bb60381a6144f0e96f
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-05 20:13:34 +0600

    feat: add primitive data types examples

---
+++ src/PH-Modules/Conceptual-Seccion/Session-1/Part-1/index.ts
@@ -0,0 +1,15 @@
+{
+    // primitive, nonprimitive, object, array, tuple, function, arrow function, type alias
+
+    // primitive data types
+
+    const userName: string = "Moshfiqur Rahman";
+
+    const userId: number = 20159;
+
+    const isActive: boolean = true;
+
+    const userData: null = null;
+
+    const userBalance: undefined = undefined;
+}
