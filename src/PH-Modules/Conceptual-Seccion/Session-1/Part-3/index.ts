7d713b733303a251400c66ccc38ce908e47b3de3
Moshfiqur Rahman <studentemam@gmail.com>
2024-11-06 09:44:56 +0600

    feat: define IAnimal interface and cat object

--- src/PH-Modules/Conceptual-Seccion/Session-1/Part-3/index.ts
+++ src/PH-Modules/Conceptual-Seccion/Session-1/Part-3/index.ts
@@ -5,6 +5,19 @@
     const message: unknown = "This is Moshfiqur Rahman";
     const messageLength = (message as string).length;
 
-    
+    // interface
+    interface IAnimal {
+        id: number;
+        name: string;
+        weight: number;
+        color: string;
+    }
+
+    const cat: IAnimal = {
+        id: 911,
+        name: "Cat",
+        weight: 1.2,
+        color: "white",
+    }
 
 }
